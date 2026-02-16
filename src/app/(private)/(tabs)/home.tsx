212c27ea44d0218e535e944f82c3f71dfaeaa4b4
Davibrasil05 <davi.brasil2005@gmail.com>
2026-02-16 18:20:26 -0400

    feat: home header and search components

--- src/app/(private)/(tabs)/home.tsx
+++ src/app/(private)/(tabs)/home.tsx
@@ -1,13 +1,6 @@
-import { Text, TouchableOpacity, View } from "react-native";
-import { useUserStore } from "../../../shared/store/user-store";
-const {logout} = useUserStore()
-
+import { HomeView } from "../../../viewModels/Home/Home.view";
 export default function Home(){
     return(
-        <View className="flex-1 items-center justify-center">
-            <TouchableOpacity onPress={logout}>
-                <Text>Logout</Text>
-            </TouchableOpacity>
-        </View>
+        <HomeView/>
     )
 }
---
+++ src/viewModels/Home/Home.view.tsx
@@ -0,0 +1,23 @@
+import { FlatList, Text, View } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context"
+import { HomeHeader } from "./components/Header"
+import { SearchInput } from "./components/SearchInput"
+
+export const HomeView = () => {
+  return (
+    <SafeAreaView edges={['top']} className="flex-1">
+       <FlatList 
+        data={[]}
+        renderItem={() => <></>}
+        ListHeaderComponent={() => (
+            <>
+                <HomeHeader/>
+                <SearchInput/>
+            </>
+        )}
+        contentContainerClassName="px-[16px] pb-[120px]"
+      />
+    </SafeAreaView>
+    
+    )
+}
