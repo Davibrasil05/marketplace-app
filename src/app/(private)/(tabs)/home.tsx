5857eaac95f2969c6a0dd5ac3017817ee1f71484
Davibrasil05 <davi.brasil2005@gmail.com>
2026-02-16 13:38:43 -0400

    feat: token revalidation

--- src/app/(private)/(tabs)/home.tsx
+++ src/app/(private)/(tabs)/home.tsx
@@ -1,7 +1,7 @@
 import { Text, TouchableOpacity, View } from "react-native";
 import { useUserStore } from "../../../shared/store/user-store";
-
 const {logout} = useUserStore()
+
 export default function Home(){
     return(
         <View className="flex-1 items-center justify-center">
